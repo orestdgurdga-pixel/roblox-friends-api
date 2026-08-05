1f8466e8d33c5af05f9d1867d9103394bc32ce38
orestdgurdga-pixel <orestdgurdga@gmail.com>
2026-08-05 23:58:26 +0200

    Update server.js

--- server.js
+++ server.js
@@ -7,24 +7,32 @@ app.get("/", (req, res) => {
     res.send("Server is running!");
 });
 
+// Знайти UserId за ніком
+app.get("/userid/:username", async (req, res) => {
+    const response = await fetch("https://users.roblox.com/v1/usernames/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            usernames: [req.params.username],
+            excludeBannedUsers: false
+        })
+    });
+
+    const data = await response.json();
+    res.json(data);
+});
+
+// Отримати друзів
 app.get("/friends/:id", async (req, res) => {
-    try {
-        const response = await fetch(
-            `https://friends.roblox.com/v1/users/${req.params.id}/friends`
-        );
+    const response = await fetch(
+        `https://friends.roblox.com/v1/users/${req.params.id}/friends`
+    );
 
-        const data = await response.json();
-
-        res.json(data);
-    } catch (e) {
-        res.status(500).json({
-            error: e.toString()
-        });
-    }
+    const data = await response.json();
+    res.json(data);
 });
 
 const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-    console.log("Started");
-});
+app.listen(PORT, () => console.log("Started"));
